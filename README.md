ce2c5b201aebc02ae2edfb438a80f5ae3aa0387d
Vavelin Kevin <vavelinkevin@gmail.com>
2016-03-17 09:01:58 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ionic Side Menu + Tabs Example
+
+This is a simple project I create from the tabs starter in Ionic. A friend ask me how to implement a side menu with a tabBar so I found this workaround pretty simple.
+
+Use just the $ionicSideMenuDelegate.
---
+++ www/js/controllers.js
@@ -0,0 +1,38 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, $ionicSideMenuDelegate) {
+    $scope.openMenu = function() {
+        $ionicSideMenuDelegate.toggleLeft();
+    };
+})
+
+.controller('ChatsCtrl', function($scope, Chats, $ionicSideMenuDelegate) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+  $scope.openMenu = function() {
+      $ionicSideMenuDelegate.toggleLeft();
+  };
+  
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope, $ionicSideMenuDelegate) {
+    $scope.openMenu = function() {
+        $ionicSideMenuDelegate.toggleLeft();
+    };
+  $scope.settings = {
+    enableFriends: true
+  };
+});
